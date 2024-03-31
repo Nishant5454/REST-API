c69eca1fb1cc7ffcf3b1dcdaf1678df81d02aa50
Nishant <bhagatnishant5454@gmail.com>
2024-03-31 18:16:12 +0530

    admin page added

--- RESTAPI/API1/auth.js
+++ RESTAPI/API1/auth.js
@@ -1,5 +1,6 @@
 import express from "express"; 
 import bodyParser from "bodyparser"; 
+import e from "express";
 const app=express(); 
 const PORT=4000;  
  let ADMIN=[]; 
@@ -20,10 +21,26 @@ const userAuthentication=(req,res,next)=>{
     const {username,password}=req.headers; 
     const user=USERS.find(u=>u.username===username&&u.password===password); 
     if(user){ 
+        req.user=user;
         next(); // MIDDLEWARE CALLED
     }
     else{ 
         res.staus(401).json({message:`AUTHENTICATION FAILED`});
     }
 
-};
+};  
+app.post('/admin/signup',(req,res)=>{  
+    const admin=req.header; 
+    const existingadmin=ADMIN.find(a=>a.username===username); 
+    if(existingadmin){ 
+        res.send(403).json({message:`ADMIN ALREADY EXIST`});
+    }
+    else{ 
+        ADMIN.push(admin); 
+        res.json({message:`ADMIN SUCSESSFULLY CREATED`})
+    }
+
+    
+
+});
+
